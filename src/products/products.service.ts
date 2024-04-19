1dd898c3b37b03b3b39bf8d3e2ca643d5f7d8387
drmiltonc <92145221+drmiltonc@users.noreply.github.com>
2024-04-19 00:53:31 -0500

    manejo de excepciones

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,182 +1,98 @@
-// Importaciones de módulos de NestJS
-import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+// Importamos las dependencias necesarias
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto'; // Importamos el DTO para crear productos
+import { UpdateProductDto } from './dto/update-product.dto'; // Importamos el DTO para actualizar productos
+import { RpcException } from '@nestjs/microservices'; // Importamos la excepción para manejar errores
+import { PrismaClient } from '@prisma/client'; // Importamos el cliente de Prisma
+import { PaginationDto } from 'src/common/dto'; // Importamos el DTO para la paginación
 
-// Importaciones de DTOs
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
-
-// Importación del cliente de Prisma
-import { PrismaClient } from '@prisma/client';
-
-// Importación del DTO de paginación
-import { PaginationDto } from 'src/common/dto';
-
-// Servicio de productos
+// Marcamos la clase como injectable
 @Injectable()
-export class ProductsService extends PrismaClient implements OnModuleInit {
+export class ProductsService extends PrismaClient implements OnModuleInit { // Extendemos la clase de PrismaClient e implementamos la interfaz OnModuleInit
 
-  private readonly logger = new Logger('Products Service'); // Variable privada para el registro
+  private readonly logger = new Logger('ProductsService');// Creamos una instancia del logger
 
-  // Método que se ejecuta al inicializar el módulo
+  // Método que se ejecuta cuando se inicializa el módulo
   onModuleInit() {
-    this.$connect(); // Conectar a la base de datos
-    this.logger.log('Database connected'); // Registrar en el log que la base de datos está conectada
+    this.$connect(); // Conectamos a la base de datos
+    this.logger.log('Database connected'); // Registramos en el logger que la base de datos está conectada
   }
 
   // Método para crear un producto
-  async create(createProductDto: CreateProductDto) {
-    try {
-      try {
-        const product = await this.product.create({
-          data: createProductDto,
-        }); // Crear un producto en la base de datos
-        this.logger.log(`Product created: ${product.id}`); // Registrar en el log que el producto fue creado
-        return product; // Devolver el producto creado
-
-      } catch (error) {
-        this.logger.error(`Error creating product: ${error}`); // Registrar en el log el error al crear el producto
-        throw error; // Lanzar el error
-      }
-
-    } finally {
-      this.$disconnect(); // Desconectar de la base de datos
-      this.logger.log('Database disconnected'); // Registrar en el log que la base de datos está desconectada
-    }
+  create(createProductDto: CreateProductDto) {
+    // Creamos un producto en la base de datos
+    return this.product.create({
+      data: createProductDto // Pasamos los datos del DTO como datos para crear el producto
+    });
   }
 
   // Método para obtener todos los productos
   async findAll(paginationDto: PaginationDto) {
+    const { page, limit } = paginationDto; // Extraemos la página y el límite de la paginación
+    const totalPages = await this.product.count({ where: { available: true } }); // Contamos los productos disponibles
+    const lastPage = Math.ceil(totalPages / limit); // Calculamos la última página
 
-    try {
-      const totalProducts = await this.product.count({
-        where: { available: true },
-      });// Obtener el número total de productos
-      this.logger.log(`Total products: ${totalProducts}`); // Registrar en el log el número total de productos
-
-      const products = await this.product.findMany({
-        where: { available: true },
-        skip: (paginationDto.page - 1) * 10, //page
-        take: paginationDto.limit, //limit
-      }); // Obtener los productos paginados
-      this.logger.log(`Products found: ${products.length}`); // Registrar en el log el número de productos encontrados
-
-      // Devolver los productos y los metadatos de la paginación
-      return {
-        meta: {
-          totalProducts,
-          totalPages: Math.ceil(totalProducts / paginationDto.limit),
-          page: paginationDto.page,
-          limit: paginationDto.limit,
-        },
-        data: { products },
+    // Obtenemos los productos de la base de datos
+    return {
+      // Devolvemos un objeto con los datos y los metadatos de la paginación
+      data: await this.product.findMany({
+        skip: (page - 1) * limit, // Saltamos los productos de las páginas anteriores
+        take: limit, // Limitamos el número de productos a devolver
+        where: { available: true } // Filtramos los productos por disponibilidad
+      }),
+      meta: { // Metadatos de la paginación
+        total: totalPages, // Total de páginas
+        page: page, // Página actual
+        lastPage: lastPage, // Última página
       }
-    }
-
-    catch (error) {
-      this.logger.error(`Error finding products: ${error}`); // Registrar en el log el error al obtener los productos
-      throw error; // Lanzar el error
-    }
-
-    finally {
-      this.$disconnect(); // Desconectar de la base de datos
-      this.logger.log('Database disconnected'); // Registrar en el log que la base de datos está desconectada
     }
   }
 
-
   // Método para obtener un producto por su ID
   async findOne(id: number) {
-    try {
-      const product = await this.product.findUnique({
-        where: { id, available: true },
-      }); // Buscar el producto en la base de datos
-      this.logger.log(`Product found: ${product.id}`); // Registrar en el log que el producto fue encontrado
-      this.logger.log(`Product name: ${product.name}`); // Registrar en el log el nombre del producto
-      this.logger.log(`Product price: ${product.price}`); // Registrar en el log el precio del producto
+    // Buscamos el producto en la base de datos
+    const product = await this.product.findFirst({
+      where: { id, available: true } // Filtramos por ID y disponibilidad
+    });
 
-      // Si el producto no existe, lanzar una excepción
-      if (!product) {
-        throw new NotFoundException('Product not found');
-      }
-
-      return product; // Devolver el producto
-
-    } catch (error) {
-      this.logger.error(`Error finding product: ${error}`); // Registrar en el log el error al obtener el producto
-      throw error; // Lanzar el error
-
-    } finally {
-      this.$disconnect(); // Desconectar de la base de datos
-      this.logger.log('Database disconnected'); // Registrar en el log que la base de datos está desconectada
+    // Si no encontramos el producto
+    if (!product) {
+      // Lanzamos una excepción
+      throw new RpcException({
+        message: `Product with id #${id} not found`, // Mensaje de error
+        status: HttpStatus.BAD_REQUEST // Código de estado de error
+      });
     }
+
+    return product; // Devolvemos el producto
   }
 
   // Método para actualizar un producto
   async update(id: number, updateProductDto: UpdateProductDto) {
+    const { id: __, ...data } = updateProductDto; // Extraemos el ID y los datos del DTO
 
-    try {
+    // Buscamos el producto en la base de datos
+    await this.findOne(id);
 
-      const {id: __, ...data} = updateProductDto;
-
-      await this.findOne(id); // Buscar el producto en la base de datos
-      this.logger.log(`Product found: ${id}`); // Registrar en el log que el producto fue encontrado
-
-      // Actualizar el producto en la base de datos
-      const product = await this.product.update({
-        where: { id },
-        data: data,
-      });
-      this.logger.log(`Product updated: ${id}`); // Registrar en el log que el producto fue actualizado
-
-      // Si el producto no existe, lanzar una excepción
-      if (!product) {
-        throw new NotFoundException('Product not found');
-      }
-
-      return product; // Devolver el producto actualizado
-
-    } catch (error) {
-      this.logger.error(`Error updating product: ${error}`); // Registrar en el log el error al actualizar el producto
-      throw error; // Lanzar el error
-
-    } finally {
-      this.$disconnect(); // Desconectar de la base de datos
-      this.logger.log('Database disconnected'); // Registrar en el log que la base de datos está desconectada
-    }
+    // Actualizamos el producto en la base de datos
+    return this.product.update({
+      where: { id }, // Filtramos por ID
+      data: data, // Pasamos los datos del DTO como datos para actualizar el producto
+    });
   }
 
   // Método para eliminar un producto
   async remove(id: number) {
 
-    try {
+    await this.findOne(id); // Buscamos el producto en la base de datos
 
-      await this.findOne(id); // Buscar el producto en la base de datos
-      this.logger.log(`Product found: ${id}`); // Registrar en el log que el producto fue encontrado
+    // Actualizamos el producto en la base de datos
+    const product = await this.product.update({
+      where: { id }, // Filtramos por ID
+      // Pasamos los datos para actualizar el producto
+      data: { available: false } // Marcamos el producto como no disponible
+    });
 
-      // Actualizar el producto en la base de datos para marcarlo como no disponible
-      const product = await this.product.update({
-        where: { id },
-        data: {
-          available: false,
-        },
-      });
-      this.logger.log(`Product removed: ${id}`); // Registrar en el log que el producto fue eliminado
-
-      // Si el producto no existe, lanzar una excepción
-      if (!product) {
-        throw new NotFoundException('Product not found');
-      }
-
-      return product; // Devolver el producto eliminado
-
-    } catch (error) {
-      this.logger.error(`Error removing product: ${error}`); // Registrar en el log el error al eliminar el producto
-      throw error; // Lanzar el error
-
-    } finally {
-      this.$disconnect(); // Desconectar de la base de datos
-      this.logger.log('Database disconnected'); // Registrar en el log que la base de datos está desconectada
-    }
-    ;
+    return product; // Devolvemos el producto actualizado
   }
 }
